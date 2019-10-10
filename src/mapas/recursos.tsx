31e75ea48b6d30473c64e5a4d825f09071027012
adriel.vieira <adriel.vieira@estagiario.ufca.edu.br>
2019-10-10 11:51:38 -0300

    Produção das fases de busca em profundidade

---
+++ src/mapas/recursos.tsx
@@ -0,0 +1,19 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="recursos" tilewidth="50" tileheight="50" tilecount="5" columns="0">
+ <grid orientation="orthogonal" width="1" height="1"/>
+ <tile id="0">
+  <image width="50" height="50" source="../assets/ferormonio.png"/>
+ </tile>
+ <tile id="1">
+  <image width="50" height="50" source="../assets/grama.png"/>
+ </tile>
+ <tile id="2">
+  <image width="50" height="50" source="../assets/arvoreGrande.png"/>
+ </tile>
+ <tile id="3">
+  <image width="50" height="50" source="../assets/arvorePequena.png"/>
+ </tile>
+ <tile id="4">
+  <image width="50" height="50" source="../assets/formigueiro.png"/>
+ </tile>
+</tileset>
