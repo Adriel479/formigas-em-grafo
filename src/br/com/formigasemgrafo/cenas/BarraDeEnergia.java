93bd9c31271482d7392fa7ad2d8220b6365ac876
adriel.vieira <adriel.vieira@estagiario.ufca.edu.br>
2019-10-14 11:29:21 -0300

    Adicionado json das fases e o tempo de vida dos formigueiros. Falta penalizar o jogador no caso da visitação errada.

---
+++ src/br/com/formigasemgrafo/cenas/BarraDeEnergia.java
@@ -0,0 +1,57 @@
+package br.com.formigasemgrafo.cenas;
+
+import java.awt.Color;
+import java.awt.Graphics2D;
+import java.awt.image.BufferedImage;
+
+import br.com.formigasemgrafo.core.Sprite;
+
+public class BarraDeEnergia extends Sprite {
+
+	private int nivelDaBarra;
+	private boolean extremo;
+	
+
+	public BarraDeEnergia(int x, int y, BufferedImage imagem) {
+		super(x, y, imagem);
+	}
+
+	@Override
+	public void renderizeme(Graphics2D g) {
+		ajustarBarraDeEnergia();
+		super.renderizeme(g);
+	}
+
+	private void ajustarBarraDeEnergia() {
+		Graphics2D g = (Graphics2D) imagem.getGraphics();
+		g.setColor(new Color(93, 58, 27));
+		g.fillRect(0, 0, imagem.getWidth(), imagem.getHeight());
+		g.setColor(new Color(128, 190, 31));
+		g.fillRect(0, 0, (int) (imagem.getWidth() * nivelDaBarra / 100.0f), imagem.getHeight());
+		g.setColor(Color.RED);
+		g.drawRect(0, 0, imagem.getWidth() - 1, imagem.getHeight() - 1);
+		g.setColor(Color.GREEN);
+		g.dispose();
+	}
+
+	public void atualizaNivelDaBarra(int reducao) {
+		this.nivelDaBarra -= reducao;
+	}
+	
+	public int getNivelDaBarra() {
+		return nivelDaBarra;
+	}
+
+	public void setNivelDaBarra(int nivelDaBarra) {
+		this.nivelDaBarra = nivelDaBarra;
+	}
+
+	public boolean isExtremo() {
+		return extremo;
+	}
+
+	public void setExtremo(boolean extremo) {
+		this.extremo = extremo;
+	}
+
+}
--- src/br/com/formigasemgrafo/cenas/BuscaEmProfundidadeFase3.java
+++ src/br/com/formigasemgrafo/cenas/BuscaEmProfundidadeFase3.java
@@ -1,14 +1,6 @@
 package br.com.formigasemgrafo.cenas;
 
-import java.awt.Graphics2D;
-import java.awt.Point;
 import java.awt.event.KeyEvent;
-import java.awt.image.BufferedImage;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
 
 import br.com.formigasemgrafo.core.Animacao;
 import br.com.formigasemgrafo.core.Camada;
@@ -17,7 +9,7 @@ import br.com.formigasemgrafo.core.MapaSprite;
 import br.com.formigasemgrafo.core.SpriteSheet;
 import br.com.formigasemgrafo.core.Util;
 
-public class BuscaEmProfundidadeFase1 extends Cena {
+public class BuscaEmProfundidadeFase3 extends Cena {
 
 	private enum Orientacao {
 		CIMA, BAIXO, DIREITA, ESQUERDA
@@ -28,56 +20,15 @@ public class BuscaEmProfundidadeFase1 extends Cena {
 	private Orientacao orientacaoDaFormiga;
 	private MapaSprite meuMapa;
 	private Camada camadaDeArvores;
-	private long tempoAnterior;
-	private Map<Point, List<Point>> mapaDeAdjacencia;
-	private Map<Point, BarraDeEnergia> mapaDeBarras;
-	private Point pontoAuxiliar;
 
 	@Override
 	public void carregar() {
-		mapa.carregarMapaDeSprite("mapaBPFase1", "/mapas/buscaEmProfundidadeFase1.json", "/mapas/recursos.tsx");
+		mapa.carregarMapaDeSprite("mapaBPFase3", "/mapas/buscaEmProfundidadeFase3.json", "/mapas/recursos.tsx");
 		imagem.carregarImagem("jogador", "/assets/spriteSheet.png");
 	}
 
 	@Override
 	public void atualizar() {
-
-		// Controla a barra dos formigueiros
-		long tempoAtual = System.currentTimeMillis();
-		if (tempoAtual - tempoAnterior > 500) {
-			for (BarraDeEnergia barraDeEnergia : mapaDeBarras.values()) {
-				if (barraDeEnergia.getNivelDaBarra() <= 0) {
-					barraDeEnergia.setVisivel(false);
-				}
-				if (barraDeEnergia.isExtremo())
-					barraDeEnergia.atualizaNivelDaBarra(3);
-				else
-					barraDeEnergia.atualizaNivelDaBarra(1);
-			}
-			tempoAnterior = tempoAtual;
-		}
-
-		for (Point ponto : mapaDeBarras.keySet()) {
-			BarraDeEnergia barra = mapaDeBarras.get(ponto);
-			if (Util.houveInterseccao("jogador", sprite, "padrao", barra) && barra.isVisivel() && barra.isExtremo()
-					&& entrada.isTeclaPressionada(KeyEvent.VK_A)) {
-				List<Point> adjacentes = mapaDeAdjacencia.get(ponto);
-
-				// Dado o (u, v) remove a adjacencia (v, u), pois u será removido do grafo
-				if (adjacentes.size() > 0)
-					mapaDeAdjacencia.get(adjacentes.get(0)).remove(ponto);
-
-				// Quando um nó 'u' só possui um ou nenhum adjacente, ele é um extremo
-				if (mapaDeAdjacencia.get(adjacentes.get(0)).size() <= 1) {
-					mapaDeBarras.get(adjacentes.get(0)).setExtremo(true);
-					mapaDeBarras.get(adjacentes.get(0)).atualizaNivelDaBarra(10);
-				}
-
-				barra.setVisivel(false);
-				break;
-			}
-		}
-
 		if (entrada.isTeclaPressionada(KeyEvent.VK_UP)) {
 			sprite.deslocarXY(0, -deslocamento);
 			if (Util.houveInterseccao(sprite, "arvores", camadaDeArvores) || Util.foraDaCena(sprite, this)) {
@@ -127,6 +78,7 @@ public class BuscaEmProfundidadeFase1 extends Cena {
 		} else if (orientacaoDaFormiga == Orientacao.DIREITA) {
 			sprite.executarAnimacao("animacaoDireitaNormal");
 		}
+
 	}
 
 	@Override
@@ -152,69 +104,13 @@ public class BuscaEmProfundidadeFase1 extends Cena {
 		sprite.executarAnimacao("animacaoCimaNormal");
 		orientacaoDaFormiga = Orientacao.CIMA;
 
-		meuMapa = mapa.getMapaDeSprite("mapaBPFase1");
+		meuMapa = mapa.getMapaDeSprite("mapaBPFase3");
 		camadaDeArvores = meuMapa.getCamada(2);
 		camadaDeArvores.criarAreaRetangular("arvores", 0, 0, 50, 50);
-
-		sprite.redimensionar(40, 40);
-		sprite.criarAreaRetangular("jogador", 0, 0, 40, 40);
-
-		mapaDeBarras = new HashMap<Point, BarraDeEnergia>();
-
-		BarraDeEnergia barra = new BarraDeEnergia(360, 570, new BufferedImage(30, 10, BufferedImage.TYPE_3BYTE_BGR));
-		barra.setNivelDaBarra(100);
-		barra.setExtremo(false);
-		barra.criarAreaRetangular("padrao", 0, 0, 30, 10);
-		mapaDeBarras.put(new Point(7, 11), barra);
-
-		barra = new BarraDeEnergia(360, 270, new BufferedImage(30, 10, BufferedImage.TYPE_3BYTE_BGR));
-		barra.setNivelDaBarra(100);
-		barra.setExtremo(false);
-		barra.criarAreaRetangular("padrao", 0, 0, 30, 10);
-		mapaDeBarras.put(new Point(7, 5), barra);
-
-		barra = new BarraDeEnergia(60, 270, new BufferedImage(30, 10, BufferedImage.TYPE_3BYTE_BGR));
-		barra.setNivelDaBarra(100);
-		barra.setExtremo(true);
-		barra.criarAreaRetangular("padrao", 0, 0, 30, 10);
-		mapaDeBarras.put(new Point(1, 5), barra);
-
-		barra = new BarraDeEnergia(760, 270, new BufferedImage(30, 10, BufferedImage.TYPE_3BYTE_BGR));
-		barra.setNivelDaBarra(100);
-		barra.setExtremo(true);
-		barra.criarAreaRetangular("padrao", 0, 0, 30, 10);
-		mapaDeBarras.put(new Point(15, 5), barra);
-
-		barra = new BarraDeEnergia(360, 120, new BufferedImage(30, 10, BufferedImage.TYPE_3BYTE_BGR));
-		barra.setNivelDaBarra(100);
-		barra.setExtremo(false);
-		barra.criarAreaRetangular("padrao", 0, 0, 30, 10);
-		mapaDeBarras.put(new Point(7, 2), barra);
-
-		barra = new BarraDeEnergia(610, 120, new BufferedImage(30, 10, BufferedImage.TYPE_3BYTE_BGR));
-		barra.setNivelDaBarra(100);
-		barra.setExtremo(true);
-		barra.criarAreaRetangular("padrao", 0, 0, 30, 10);
-		mapaDeBarras.put(new Point(12, 2), barra);
-
 		adicionarObjetoRenderizavel(meuMapa);
-		for (BarraDeEnergia barraE : mapaDeBarras.values())
-			adicionarObjetoRenderizavel(barraE);
 		adicionarObjetoRenderizavel(sprite);
 
-		mapaDeAdjacencia = new HashMap<Point, List<Point>>();
-		mapaDeAdjacencia.put(new Point(7, 11), new ArrayList<Point>(Arrays.asList(new Point(7, 5))));
-		mapaDeAdjacencia.put(new Point(7, 5), new ArrayList<Point>(
-				Arrays.asList(new Point(7, 11), new Point(1, 5), new Point(15, 5), new Point(7, 2))));
-		mapaDeAdjacencia.put(new Point(1, 5), new ArrayList<Point>(Arrays.asList(new Point(7, 5))));
-		mapaDeAdjacencia.put(new Point(15, 5), new ArrayList<Point>(Arrays.asList(new Point(7, 5))));
-		mapaDeAdjacencia.put(new Point(7, 2), new ArrayList<Point>(Arrays.asList(new Point(12, 2), new Point(7, 5))));
-		mapaDeAdjacencia.put(new Point(12, 2), new ArrayList<Point>(Arrays.asList(new Point(7, 2))));
-	}
-
-	@Override
-	public void renderizar(Graphics2D g) {
-		super.renderizar(g);
+		sprite.redimensionar(40, 40);
 	}
 
 	@Override
