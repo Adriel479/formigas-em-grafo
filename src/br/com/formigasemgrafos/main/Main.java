6e58bff7526c999290cfaeda1556999c42e82718
Adriel479 <adriel.klt@gmail.com>
2019-10-16 15:21:20 -0300

    Adiconado Fase 3

--- src/br/com/formigasemgrafos/main/Main.java
+++ src/br/com/formigasemgrafos/main/Main.java
@@ -4,6 +4,7 @@ import java.io.FileNotFoundException;
 
 import br.com.formigasemgrafo.cenas.BuscaEmProfundidadeFase1;
 import br.com.formigasemgrafo.cenas.BuscaEmProfundidadeFase2;
+import br.com.formigasemgrafo.cenas.BuscaEmProfundidadeFase3;
 import br.com.formigasemgrafo.cenas.DefinicaoDeGrafo;
 import br.com.formigasemgrafo.cenas.Desafios;
 import br.com.formigasemgrafo.cenas.MapaAlimentacao;
@@ -22,11 +23,11 @@ public class Main {
 		formigasEmGrafo.adicionarCena("mapaProtecao", new MapaProtecao());
 		formigasEmGrafo.adicionarCena("fase1BuscaEmProfundidade", new BuscaEmProfundidadeFase1());
 		formigasEmGrafo.adicionarCena("fase2BuscaEmProfundidade", new BuscaEmProfundidadeFase2());
-//		formigasEmGrafo.adicionarCena("fase3BuscaEmProfundidade", new BuscaEmProfundidadeFase3());
+		formigasEmGrafo.adicionarCena("fase3BuscaEmProfundidade", new BuscaEmProfundidadeFase3());
 //		formigasEmGrafo.adicionarCena("fase4BuscaEmProfundidade", new BuscaEmProfundidadeFase4());
 //		formigasEmGrafo.adicionarCena("fase5BuscaEmProfundidade", new BuscaEmProfundidadeFase5());
 //		formigasEmGrafo.adicionarCena("fase6BuscaEmProfundidade", new BuscaEmProfundidadeFase6());
-		formigasEmGrafo.setCenaAtual("menuPrincipal");
+		formigasEmGrafo.setCenaAtual("fase3BuscaEmProfundidade");
 		formigasEmGrafo.executarJogo();
 	}
 
